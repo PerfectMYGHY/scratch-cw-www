ff9df778c40db7b75fe40a9823c1871816b2f79b
PerfectMYGHY <916881890@qq.com>
2026-06-30 14:04:48 +0800

    chore: 修改了很多

--- src/views/credits/credits.jsx
+++ src/views/credits/credits.jsx
@@ -3,77 +3,109 @@ const render = require('../../lib/render.jsx');
 const FormattedMessage = require('react-intl').FormattedMessage;
 const injectIntl = require('react-intl').injectIntl;
 const Page = require('../../components/page/www/page.jsx');
-const People = require('./people.json');
 const PeopleGrid = require('../../components/people-grid/people-grid.jsx');
 
 require('./credits.scss');
 
-// Sort people by name alphabetically
-People.sort((a, b) => (a.name.localeCompare(b.name)));
+class Credits extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            people: [],
+            error: null,
+            loading: true
+        };
+    }
 
-const Credits = () => (
-    <div className="credits">
-        <div className="content">
-            <div className="people">
-                <div className="mid-header">
-                    <h2><FormattedMessage id="credits.ourteam" /></h2>
-                    <p>
-                        <FormattedMessage id="credits.developers" />
-                    </p>
+    componentDidMount () {
+        fetch(`${process.env.PROJECT_HOST}/our_team/people/`).then(response => response.json())
+            .then(people => {
+                // Sort people by name alphabetically
+                people.sort((a, b) => (a.name.localeCompare(b.name)));
+                this.setState({
+                    people: people,
+                    loading: false
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    error: error.message,
+                    loading: false
+                });
+            });
+    }
+
+    render () {
+        const {people, loading, error} = this.state;
+
+        return (
+            <div className="credits">
+                <div className="content">
+                    <div className="people">
+                        <div className="mid-header">
+                            <h2><FormattedMessage id="credits.ourteam" /></h2>
+                            <p>
+                                <FormattedMessage id="credits.developers" />
+                            </p>
+                        </div>
+                        {loading && (<p style={{textAlign: 'center'}}>正在加载...</p>)}
+                        {error && (<p style={{textAlign: 'center'}}>加载失败，错误信息: {error}</p>)}
+                        {people && (<PeopleGrid people={people} />)}
+                    </div>
                 </div>
-                <PeopleGrid people={People} />
+                <div
+                    className="content"
+                    id="acknowledgements"
+                >
+                    <div className="acknowledge-content">
+                        <h2>
+                            <FormattedMessage id="credits.codeWriterTitle" />
+                        </h2>
+                        <p>
+                            <FormattedMessage
+                                id="credits.introduceCodeWriter"
+                                values={{
+                                    writerLink: (
+                                        <a
+                                            href="/users/webmaster/"
+                                            target="_blank"
+                                        >
+                                            <FormattedMessage id="credits.webmaster" />
+                                        </a>
+                                    ),
+                                    emailLink: (
+                                        <a>
+                                            <FormattedMessage id="credits.email" />
+                                        </a>
+                                    )
+                                }}
+                            />
+                        </p>
+                        <h2>
+                            <FormattedMessage id="credits.donorsTitle" />
+                        </h2>
+                        <p>
+                            <FormattedMessage
+                                id="credits.acknowledgementsDonors"
+                                values={{
+                                    donorsLink: (
+                                        <a
+                                            href="/sponsor"
+                                            rel="noreferrer noopener"
+                                            target="_blank"
+                                        >
+                                            <FormattedMessage id="credits.acknowledgementsDonorsLinkText" />
+                                        </a>
+                                    )
+                                }}
+                            />
+                        </p>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div
-            className="content"
-            id="acknowledgements"
-        >
-            <div className="acknowledge-content">
-                <h2>
-                    <FormattedMessage id="credits.codeWriterTitle" />
-                </h2>
-                <p>
-                    <FormattedMessage
-                        id="credits.introduceCodeWriter"
-                        values={{
-                            writerLink: (
-                                <a
-                                    href="/users/webmaster/"
-                                    target="_blank"
-                                >
-                                    <FormattedMessage id="credits.webmaster" />
-                                </a>
-                            ),
-                            emailLink: (
-                                <a>
-                                    <FormattedMessage id="credits.email" />
-                                </a>
-                            )
-                        }}
-                    />
-                </p>
-                <h2>
-                    <FormattedMessage id="credits.donorsTitle" />
-                </h2>
-                <p>
-                    <FormattedMessage
-                        id="credits.acknowledgementsDonors"
-                        values={{
-                            donorsLink: (
-                                <a
-                                    href="/sponsor"
-                                    rel="noreferrer noopener"
-                                    target="_blank"
-                                >
-                                    <FormattedMessage id="credits.acknowledgementsDonorsLinkText" />
-                                </a>
-                            )
-                        }}
-                    />
-                </p>
-            </div>
-        </div>
-    </div>);
+        );
+    }
+}
 
 const WrappedCredits = injectIntl(Credits);
 render(<Page><WrappedCredits /></Page>, document.getElementById('app'));
